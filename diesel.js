7cbc6f50ced0ec6464a645765c09768172ee2d63
William Wang <defund@users.noreply.github.com>
2020-08-20 18:42:08 -0400

    more json parsing

--- diesel.js
+++ diesel.js
@@ -1,5 +1,4 @@
 const messages_route = /^https:\/\/discord(app)?\.com\/api\/v8\/channels\/[0-9]+\/messages$/
-
 const nitro_emoji = /<((a)?:[a-zA-Z0-9]{2,}:[0-9]+)>/
 const diesel_emoji = /<((a)?:[a-zA-Z0-9]{2,}:[0-9]+)#diesel>/
 
@@ -7,6 +6,9 @@ const encode = (content) => content.replace(nitro_emoji, (_, id) => `<${id}#dies
 const decode = (content) => content.replace(diesel_emoji, (_, id) => `<${id}>`)
 
 const xml_open_old = XMLHttpRequest.prototype.open
+const xml_send_old = XMLHttpRequest.prototype.send
+const json_parse_old = JSON.parse
+
 XMLHttpRequest.prototype.open = function() {
 	if (arguments[1].match(messages_route)) {
 		this._diesel = true
@@ -14,26 +16,25 @@ XMLHttpRequest.prototype.open = function() {
 	xml_open_old.apply(this, arguments)
 }
 
-const xml_send_old = XMLHttpRequest.prototype.send
 XMLHttpRequest.prototype.send = function() {
-	console.log(arguments)
 	if (this._diesel && typeof arguments[0] == 'string') {
-		const body = JSON.parse(arguments[0])
+		const body = json_parse_old(arguments[0])
 		body.content = encode(body.content)
 		arguments[0] = JSON.stringify(body)
 	} else if (this._diesel && arguments[0] instanceof FormData) {
 		const content = arguments[0].get('content')
 		arguments[0].set('content', encode(content))
 	}
-	console.log('done')
 	xml_send_old.apply(this, arguments)
 }
 
-const json_parse_old = JSON.parse
+
 JSON.parse = function() {
 	const object = json_parse_old.apply(this, arguments)
 	if (object.t == 'MESSAGE_CREATE') {
 		object.d.content = decode(object.d.content)
+	} else if (typeof object.content == 'string') {
+		object.content = decode(object.content)
 	} else if (object instanceof Array) {
 		if (object.length > 0 && typeof object[0].content == 'string') {
 			object.forEach(message => message.content = decode(message.content))
