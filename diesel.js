4508a66aea6c170e20120c199671201e8697f22a
William Wang <defund@users.noreply.github.com>
2020-08-21 16:57:39 -0400

    webpack

--- diesel.js
+++ diesel.js
@@ -1,44 +1,66 @@
-const messages_route = /^https:\/\/discord(app)?\.com\/api\/v8\/channels\/[0-9]+\/messages$/
-const nitro_emoji = /<((a)?:[a-zA-Z0-9]{2,}:[0-9]+)>/
-const diesel_emoji = /<((a)?:[a-zA-Z0-9]{2,}:[0-9]+)#diesel>/
+const getModuleWithMethod = (method) => webpackJsonp
+	.push([0,{a:(m,e,{c})=>e.x=Object.values(c)},['a']]).x
+	.find(s=>s.exports?.default?.[method]).exports.default
 
-const encode = (content) => content.replace(nitro_emoji, (_, id) => `<${id}#diesel>`)
-const decode = (content) => content.replace(diesel_emoji, (_, id) => `<${id}>`)
+window.onload = () => {
+	const banner = '#diesel'
+	const encodeRegex = /<(a?:\w+:)(\d+)>/g
+	const decodeRegex = /<(a?:\w+:)(\d+)#diesel>/g
 
-const xml_open_old = XMLHttpRequest.prototype.open
-const xml_send_old = XMLHttpRequest.prototype.send
-const json_parse_old = JSON.parse
+	const dispatchModule = getModuleWithMethod('dispatch')
+	const messageModule = getModuleWithMethod('sendMessage')
 
-XMLHttpRequest.prototype.open = function() {
-	if (arguments[1].match(messages_route)) {
-		this._diesel = true
-	}
-	xml_open_old.apply(this, arguments)
-}
+	const dispatch = dispatchModule.dispatch
+	const sendMessage = messageModule.sendMessage
 
-XMLHttpRequest.prototype.send = function() {
-	if (this._diesel && typeof arguments[0] == 'string') {
-		const body = json_parse_old(arguments[0])
-		body.content = encode(body.content)
-		arguments[0] = JSON.stringify(body)
-	} else if (this._diesel && arguments[0] instanceof FormData) {
-		const content = arguments[0].get('content')
-		arguments[0].set('content', encode(content))
-	}
-	xml_send_old.apply(this, arguments)
-}
-
-
-JSON.parse = function() {
-	const object = json_parse_old.apply(this, arguments)
-	if (object.t == 'MESSAGE_CREATE') {
-		object.d.content = decode(object.d.content)
-	} else if (typeof object.content == 'string') {
-		object.content = decode(object.content)
-	} else if (object instanceof Array) {
-		if (object.length > 0 && typeof object[0].content == 'string') {
-			object.forEach(message => message.content = decode(message.content))
+	const encodeMessage = (message) => {
+		if (typeof message?.content == 'string' &&
+			message?.invalidEmojis instanceof Array) {
+			const ids = message.invalidEmojis.map((emoji) => emoji.id)
+			message.invalidEmojis.length = 0
+			message.content = message.content
+				.replace(encodeRegex, (match, name, id) => {
+					if (ids.includes(id)) {
+						return `<${name}${id}${banner}>`
+					}
+					return match
+				})
 		}
 	}
-	return object;
+
+	const decodeMessage = (message) => {
+		if (typeof message?.content == 'string') {
+			console.log('[b]', message.content)
+			message.content = message.content
+				.replace(decodeRegex, (_, name, id) => `<${name}${id}>`)
+			console.log('[q]', message.content)
+		}
+	}
+
+	dispatchModule.__proto__.dispatch = function() {
+		const event = arguments[0]
+		if (event?.type == 'MESSAGE_CREATE') {
+			decodeMessage(event.message)
+		} else if (event?.type == 'LOAD_MESSAGES_SUCCESS') {
+			if (event?.messages instanceof Array) {
+				event.messages.forEach(decodeMessage)
+			}
+		} else if (event?.type == 'SEARCH_FINISH') {
+			if (event?.messages instanceof Array) {
+				event.messages.forEach((preview) => {
+					if (preview instanceof Array) {
+						preview.forEach(decodeMessage)
+					}
+				})
+			}
+		}
+		return dispatch.apply(this, arguments)
+	}
+
+	messageModule.sendMessage = function() {
+		const message = arguments[1]
+		encodeMessage(message)
+		console.log('[+]', message)
+		return sendMessage.apply(this, arguments)
+	}
 }
