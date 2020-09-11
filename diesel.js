284f4ae036483eeb4b0d5b5d088909ea265b810c
William Wang <defund@users.noreply.github.com>
2020-09-10 22:50:18 -0400

    no more optional chaining

--- diesel.js
+++ diesel.js
@@ -1,11 +1,15 @@
 const getModuleWithMethod = (method) => webpackJsonp
 	.push([0,{a:(m,e,{c})=>e.x=Object.values(c)},['a']]).x
-	.find(s=>s.exports?.default?.[method]).exports.default
+	.find(s=>s.exports.default?s.exports.default[method]:null)
+	.exports.default
 
 window.onload = () => {
-	const banner = '#diesel'
+	const flag = '#<https://github.com/defund/diesel>'
 	const encodeRegex = /<(a?:\w+:)(\d+)>/g
-	const decodeRegex = /<(a?:\w+:)(\d+)#diesel>/g
+	const decodeRegex = /<(a?:\w+:)(\d+)#<https:\/\/github\.com\/defund\/diesel>>/g
+
+	var dieselUser
+	var dieselNozzle = true
 
 	const channelModule = getModuleWithMethod('getChannel')
 	const customEmojiModule = getModuleWithMethod('getCustomEmojiById')
@@ -18,61 +22,68 @@ window.onload = () => {
 	const sendMessage = messageModule.sendMessage
 	const getCurrentUser = userModule.getCurrentUser
 
-	var dieselToggle = false;
-	var dieselUser;
-
-	userModule.__proto__.getCurrentUser = function() {
-		const user = getCurrentUser.apply(this, arguments)
-		if (dieselToggle) {
-			dieselToggle = false;
-			return user;
-		}
-		if (!dieselUser) {
-			dieselUser = Object.create(Object.getPrototypeOf(user))
-		}
-		Object.assign(dieselUser, user)
-		dieselUser.premiumType = 2
-		return dieselUser
+	const isEmojiDisabled = (emoji, channel) => {
+		dieselNozzle = false
+		const result = emojiModule.isEmojiDisabled(emoji, channel)
+		console.log(result)
+		dieselNozzle = true
+		return result
 	}
 
 	const decodeMessage = (message) => {
-		if (typeof message?.content == 'string') {
+		if (message && typeof message.content == 'string') {
 			message.content = message.content
 				.replace(decodeRegex, (_, name, id) => `<${name}${id}>`)
 		}
 	}
 
-	dispatchModule.__proto__.dispatch = function() {
-		const event = arguments[0]
-		if (event?.type == 'MESSAGE_CREATE') {
-			decodeMessage(event.message)
-		} else if (event?.type == 'LOAD_MESSAGES_SUCCESS') {
-			if (event?.messages instanceof Array) {
-				event.messages.forEach(decodeMessage)
+	userModule.__proto__.getCurrentUser = function() {
+		const user = getCurrentUser.apply(this, arguments)
+		if (dieselNozzle) {
+			if (!dieselUser) {
+				dieselUser = Object.create(Object.getPrototypeOf(user))
 			}
-		} else if (event?.type == 'SEARCH_FINISH') {
-			if (event?.messages instanceof Array) {
-				event.messages.forEach((preview) => {
-					if (preview instanceof Array) {
-						preview.forEach(decodeMessage)
-					}
-				})
+			Object.assign(dieselUser, user)
+			dieselUser.premiumType = 2
+			return dieselUser
+		}
+		return user;
+	}
+
+	dispatchModule.__proto__.dispatch = function() {
+		if (arguments[0]) {
+			const event = arguments[0]
+			if (event.type == 'MESSAGE_CREATE') {
+				decodeMessage(event.message)
+			} else if (event.type == 'LOAD_MESSAGES_SUCCESS') {
+				if (event.messages instanceof Array) {
+					event.messages.forEach(decodeMessage)
+				}
+			} else if (event.type == 'SEARCH_FINISH') {
+				if (event.messages instanceof Array) {
+					event.messages.forEach((preview) => {
+						if (preview instanceof Array) {
+							preview.forEach(decodeMessage)
+						}
+					})
+				}
 			}
 		}
 		return dispatch.apply(this, arguments)
 	}
 
 	messageModule.sendMessage = function() {
-		const message = arguments[1]
-		if (typeof message?.content == 'string') {
-			const channel = channelModule.getChannel(arguments[0])
-			message.content = message.content
-				.replace(encodeRegex, (match, name, id) => {
-					dieselToggle = true
-					const emoji = customEmojiModule.getCustomEmojiById(id)
-					return emojiModule.isEmojiDisabled(emoji, channel) ?
-						`<${name}${id}${banner}>` : match
-				})
+		if (arguments[1]) {
+			const message = arguments[1]
+			if (typeof message.content == 'string') {
+				const channel = channelModule.getChannel(arguments[0])
+				message.content = message.content
+					.replace(encodeRegex, (match, name, id) => {
+						const emoji = customEmojiModule.getCustomEmojiById(id)
+						return isEmojiDisabled(emoji, channel) ?
+							`<${name}${id}${flag}>` : match
+					})
+			}
 		}
 		return sendMessage.apply(this, arguments)
 	}
